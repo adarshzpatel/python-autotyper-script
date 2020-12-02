b30a62574b2c3f23a4b798a85187777e31d2c78d
adarshzpatel <adarshzpatel@gmail.com>
2020-12-02 06:06:03 +0530

    Added Documentation to the code

--- python-autotyper-script.py
+++ python-autotyper-script.py
@@ -1,11 +1,15 @@
-import pyautogui,time
+import pyautogui #Install pyautogui by typing " pip install pyautogui " in the command line. 
+import time
 
+#time delay after which script runs after starting the program
 time.sleep(4);
 
-text = "Folly words widow one downs few age every seven. If miss part by fact he park just shew. Discovered had get considered projection who favourable. Necessary up knowledge it tolerably. Unwilling departure education is be dashwoods or an. Use off agreeable law unwilling sir deficient curiosity instantly. Easy mind life fact with see has bore ten. Parish any chatty can elinor direct for former. Up as meant widow equal an share least. Another journey chamber way yet females man. Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard. Village did removed enjoyed explain nor ham saw calling talking. Securing as informed declared or margaret."
+text = "Place your text here" #inside double quotes
 
-pyautogui.PAUSE = 0.01
-wordList = [ i for i in text ]
+pyautogui.PAUSE = 0.01  #Pause between each key press in miliseconds, increase to get slow wpm, decrease to get high wpm
 
-for word in wordList:
-    pyautogui.typewrite(word);
+charList = [ i for i in text ]
+
+for char in charList:
+    pyautogui.typewrite(char)
+                        
