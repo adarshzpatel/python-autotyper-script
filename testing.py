36ffe022e2350af6d7c2251062c95281e7622907
tcvscheppingen <t.c.v.scheppingen@soverin.net>
2021-01-08 21:08:26 +0100

    Found a solution for Selenium retrieving empty strings when webscraping

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 # This file is used to test/experiment with new functionality and potential bug fixes before implementing them in the main file
 from selenium import webdriver
+import time
 
 # Opens MonkeyType in a Chromemium/Chrome window
 browser = webdriver.Chrome(executable_path='chromedriver')
@@ -7,15 +8,16 @@ browser.get('https://monkeytype.com/')
 
 
 def find_words():
-    # This function is used to find a fix for Selenium not being able to retrieve all words in the HTML source
     words = browser.find_elements_by_class_name('word')
 
     word_list = []
 
     for word in words:
-        if len(word.text) < 1:
-            break
-        else:
-            word_list.append(word.text)
+        word_list.append(word.get_attribute('textContent'))
 
-    print(word_list)
+    return word_list
+
+
+time.sleep(10)
+
+find_words()
