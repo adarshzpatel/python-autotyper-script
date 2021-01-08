31135571fc3c2e0de273624895619028e658447c
tcvscheppingen <t.c.v.scheppingen@soverin.net>
2021-01-08 15:43:31 +0100

    Added function for typing a given text on MonkeyType custom text mode. Cleaned up code

--- python-autotyper-script.py
+++ python-autotyper-script.py
@@ -6,25 +6,38 @@ from datetime import datetime
 import requests
 from selenium import webdriver
 
-browser = webdriver.Chrome(executable_path='chromedriver')
-browser.get('https://monkeytype.com/')
 
 # Uses the current time as a seed for randomised typing speed
-# This method is outdated as of Python 3.9
+# This method is outdated as of Python 3.9, so I have to find a different solution for seeding.
 random.seed(datetime.now())
 
-# time delay after which script runs after starting the program
-# This is used to allow the user some time to switch to the typing test window
-time.sleep(5)
+browser = webdriver.Chrome(executable_path='chromedriver')
+# Opens MonkeyType in a Chromemium/Chrome window
+browser.get('https://monkeytype.com/')
 
 text = ""  # Enter the text to type inside the double quotes, this is used in the custom text mode on MonkeyType
 
 
+def type_given_text(text_to_type):
+    # time delay after which script runs after starting the program
+    # This is used to allow the user some time to switch to the typing test window
+    time.sleep(5)
+    char_list = [i for i in text_to_type]
+
+    for char in char_list:
+        # Uncomment line below for randomised typing speed.
+        # get_random_pause()
+        pyautogui.typewrite(char)
+
+
 def type_randomised_text():
+
     # Finds all words on current typing test, by going through the source of the page
     words = browser.find_elements_by_class_name('word')
 
     for i in range(len(words)):
+        # Uncomment line below for randomised typing speed.
+        # get_random_pause()
         # Calls the type_word() for every word in the typing test
         type_single_word(i)
 
@@ -32,21 +45,30 @@ def type_randomised_text():
 def type_single_word(i):
     word = browser.find_element_by_xpath(
         '// *[@id="words"]/div[{}]'.format(i+1))
+    print(word.text)
+
     for char in word.text:
-        # Use either get_random_pause() or get_given_pause() to set the time paused between typing each character.
-        # Using higher values for these functions decreases the typing speed and using lower values increases the typing speed.
-        # get_random_pause()
-        # get_given_pause()
         pyautogui.typewrite(char)
+
     pyautogui.typewrite(' ')
 
 
 def get_random_pause(lower_bound, upper_bound):
+    if lower_bound > upper_bound:
+        print('Lower bound cannot be greater than upper bound')
+        return
     # Sets the pause between each character to a random float between two bounds of 0.0N seconds
     # This makes the typing consistency more believable.
     pyautogui.PAUSE = float(random.randint(lower_bound, upper_bound) / 100)
 
 
 def get_given_pause(pause_time):
+    if pause_time < 0:
+        print('Pause time cannot be less than 0')
+        return
     # Sets the pause between each character to a given number of 0.0N seconds
     pyautogui.PAUSE = float(pause_time / 100)
+
+
+get_given_pause(0)
+type_randomised_text()
