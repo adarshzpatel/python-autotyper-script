32874e88333a53a13ba78a2846db4a600dc2babe
tcvscheppingen <t.c.v.scheppingen@soverin.net>
2021-01-08 13:52:56 +0100

    add chromedriver and started working on automatically typing words generated by MonkeyType

--- python-autotyper-script.py
+++ python-autotyper-script.py
@@ -3,12 +3,37 @@ import pyautogui
 import time
 import random
 from datetime import datetime
+import requests
+from selenium import webdriver
+
+browser = webdriver.Chrome(executable_path='chromedriver')
+browser.get('https://monkeytype.com/')
+
 random.seed(datetime.now())
 
 # time delay after which script runs after starting the program
 time.sleep(5)
 
 text = ""  # Enter the text to type inside the double quotes
+
+
+def type_text():
+    words = browser.find_elements_by_class_name('word')
+
+    for i in range(len(words)):
+        type_word(i)
+
+
+def type_word(i):
+    word = browser.find_element_by_xpath(
+        '// *[@id="words"]/div[{}]'.format(i+1))
+    for char in word.text:
+        # Use either get_random_pause() or get_given_pause() to set the time paused between typing each character.
+        # Using higher values for these functions decreases the typing speed and using lower values increases the typing speed.
+        # get_random_pause()
+        # get_given_pause()
+        pyautogui.typewrite(char)
+    pyautogui.typewrite(' ')
 
 
 def get_random_pause(lower_bound, upper_bound):
@@ -22,13 +47,5 @@ def get_given_pause(pause_time):
     pyautogui.PAUSE = float(pause_time / 100)
 
 
-def type_text(text_to_type):
-
-    char_list = [i for i in text_to_type]
-
-    for char in char_list:
-        # Use either get_random_pause() or get_given_pause() to set the time paused between typing each character.
-        # Using higher values for these functions decreases the typing speed and using lower values increases the typing speed.
-        # get_random_pause()
-        # get_given_pause()
-        pyautogui.typewrite(char)
+get_given_pause(5)
+type_text()
