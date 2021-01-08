92de0640cd634b514b5b16302f9b32edd0aaa9ea
tcvscheppingen <t.c.v.scheppingen@soverin.net>
2021-01-08 15:00:32 +0100

    Added comments to the coded and cleaned up function names

--- python-autotyper-script.py
+++ python-autotyper-script.py
@@ -9,22 +9,27 @@ from selenium import webdriver
 browser = webdriver.Chrome(executable_path='chromedriver')
 browser.get('https://monkeytype.com/')
 
+# Uses the current time as a seed for randomised typing speed
+# This method is outdated as of Python 3.9
 random.seed(datetime.now())
 
 # time delay after which script runs after starting the program
+# This is used to allow the user some time to switch to the typing test window
 time.sleep(5)
 
-text = ""  # Enter the text to type inside the double quotes
+text = ""  # Enter the text to type inside the double quotes, this is used in the custom text mode on MonkeyType
 
 
-def type_text():
+def type_randomised_text():
+    # Finds all words on current typing test, by going through the source of the page
     words = browser.find_elements_by_class_name('word')
 
     for i in range(len(words)):
-        type_word(i)
+        # Calls the type_word() for every word in the typing test
+        type_single_word(i)
 
 
-def type_word(i):
+def type_single_word(i):
     word = browser.find_element_by_xpath(
         '// *[@id="words"]/div[{}]'.format(i+1))
     for char in word.text:
@@ -45,7 +50,3 @@ def get_random_pause(lower_bound, upper_bound):
 def get_given_pause(pause_time):
     # Sets the pause between each character to a given number of 0.0N seconds
     pyautogui.PAUSE = float(pause_time / 100)
-
-
-get_given_pause(5)
-type_text()
